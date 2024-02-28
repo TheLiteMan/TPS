027dc4510533128ab81742d74ac0eaa931a9e13f
TheLiteMan <ionov7077@gmail.com>
2024-02-28 20:40:20 +0300

    v0.1

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
+
+    public float RotationSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
     }
 }
