f393f818716dde3ae255402e3acf9c909a77c313
TheLiteMan <ionov7077@gmail.com>
2024-03-04 15:53:23 +0300

    v0.1.3

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravity = 9.8f;
+    public float jumpForce;
+
+    private float _fallVelocity = 0;
+    private CharacterController _charecterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charecterController = GetComponent<CharacterController>();   
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _charecterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);     
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _charecterController.isGrounded)
+        {
+            _fallVelocity = -jumpForce;
+        }
+    }
+}
