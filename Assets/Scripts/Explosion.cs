ef8052bc3c09c6432ff945a9ecedc6b57693443d
TheLiteMan <ionov7077@gmail.com>
2024-03-23 22:05:57 +0300

    0.1.12

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+    public float damage = 50;
+    public float maxSize;
+    public float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += Vector3.one * Time.deltaTime * speed;
+        if (transform.localScale.x > maxSize)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var playerHealth = other.GetComponent<PlayerHealth>();
+        if (playerHealth != null)
+        {
+            playerHealth.DealDamage(damage);
+        }
+
+        var enemyHealth = other.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.DealDamage(damage);
+        }
+    }
+}
